8b793a9da8b6cf1adc8d2e0d70d5c5af40884308
Jayaprakash121 <jayaprakashvoolla121@gmail.com>
2025-04-25 16:35:35 +0530

    update

--- yolov11m/yolov11m_ArcFace.py
+++ yolov11m/yolov11m_ArcFace.py
@@ -4,9 +4,9 @@ from ultralytics import YOLO
 from deepface import DeepFace
 import time
 
-db_path = "new_database_2/"
+db_path = "new_database/"
 
-cap = cv2.VideoCapture('video3.mp4')
+cap = cv2.VideoCapture('video3_hd.mp4')
 #cap.set(cv2.CAP_PROP_BUFFERSIZE, 0)
 model = YOLO("yolov11m-face.pt").to('cuda')
 
@@ -29,6 +29,12 @@ while True:
             confid = round(float(conf), 2)
             x, y, x1, y1 = map(int, box)
 
+            padding = 30
+            x = x - padding
+            y = y - padding
+            x1 = x1 + padding
+            y1 = y1 + padding
+
             face = frame[y:y1, x:x1]  # Crop the detected face
 
             if face.size == 0:
@@ -36,11 +42,21 @@ while True:
 
             identity = ' '
 
-            recognition_results = DeepFace.find(img_path=face, db_path=db_path, model_name="ArcFace", detector_backend='ssd',
-                                                enforce_detection=False)
+            recognition_results = DeepFace.find(
+                img_path=face,
+                db_path=db_path,
+                model_name="ArcFace",
+                enforce_detection=False,
+                detector_backend='yolov8',
+                align=True,
+                threshold=0.65,
+                normalization='ArcFace'
+            )
+
+            face_rec_conf = 0
             if len(recognition_results[0]) > 0:
                 face_rec_conf = recognition_results[0]["distance"].values[0]
-                if face_rec_conf < 0.5:
+                if face_rec_conf < 0.65:
                     identity = recognition_results[0]["identity"].values[0].split("/")[-1].split(".")[0]
                 else:
                     identity = "Unknown"
@@ -52,18 +68,16 @@ while True:
                     identity = folder
                     break
 
-
             if identity == ' ':
                 identity = 'Unknown'
             
-            identity_confidence = identity + '  ' + str(confid)
+            identity_confidence = identity + '  ' + str(round(float(face_rec_conf), 3))
             if identity == 'Unknown':
                 cv2.rectangle(frame, (x, y), (x1, y1), (255, 0, 0), 2)
                 cv2.putText(frame, identity_confidence, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
             else:
                 cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
                 cv2.putText(frame, identity_confidence, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
-
 
     cv2.putText(frame, f'FPS: {int(fps)}', (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
     cv2.imshow("Live Face Recognition - YOLOv11m_ArcFace", frame)
